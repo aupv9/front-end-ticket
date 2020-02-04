348005c8054faca9729c7afebd7b4a452f5ac31a
AuPhan <aupv96@gmail>
2020-02-04 17:10:05 +0700

    update 4/2

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,9 +1,69 @@
 import React from 'react';
+//import component material
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import { makeStyles } from '@material-ui/core/styles';
+
+//import icon material
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+
+const useStyles = makeStyles(theme => ({
+
+}));
+
 
 const SearchForm = () => {
     return (
         <div>
+            <Box p={1} display="flex" flexDirection="row"  m={1} bgcolor="background.paper">
+                <Box flexGrow={6} flexDirection="row" display="flex" >
+                    <Box p={1}  flexGrow={1} flexDirection="row" display="flex" >
+                        <Box  flexGrow={1}>
+                            <TextField
+                                id="input-with-icon-textfield"
+                                label="Nhập nơi đi"
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <LocationOnIcon  color="primary"/>
+                                        </InputAdornment>
+                                    ),
+                                }}
+                            />
+                        </Box>
+                        <Box  flexGrow={1} style={{display:"none"}}>
+                            Item 1
+                        </Box>
+                    </Box>
+                    <Box p={1}  flexGrow={1}>
+                        <Box  flexGrow={1}>
+                            <TextField
+                                id="input-with-icon-textfield"
+                                label="Nhập nơi đến"
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <LocationOnIcon  color="primary"/>
+                                        </InputAdornment>
+                                    ),
+                                }}
+                            />
+                        </Box>
+                        <Box  flexGrow={1} style={{display:"none"}}>
+                            Item 1
+                        </Box>
+                    </Box>
+                    <Box p={1}  flexGrow={1}>
+                        Item 1
+                    </Box>
+                </Box>
+                <Box p={1} bgcolor="grey.300" flexGrow={1}>
+                    Item 1
+                </Box>
 
+            </Box>
         </div>
     );
 };
--- src/views/HomeMain/HomeMain.js
+++ src/views/HomeMain/HomeMain.js
@@ -5,20 +5,27 @@ import { makeStyles } from '@material-ui/core/styles';
 import SearchForm from "../../components/SearchForm/SearchForm";
 
 
-
-
 const useStyles = makeStyles(theme => ({
     titleForm:{
         fontSize:"34px",
+        marginLeft:"10px"
+    },
+    container:{
+        position:"absolute",
+        left: "50%",
+        top: "50%",
+        transform: "translate(-50%, -50%)",
+        color:"white",
+        justifyContent:"center",
+        alignItems:"center",
+        textAlign:"center",
+        height: "50%",
 
     }
 }));
 
-
-
 const HomeMain = props => {
     const classes=useStyles();
-
 
     return (
         <div>
@@ -32,16 +39,7 @@ const HomeMain = props => {
                     style={{width:"100%",height: "100%",verticalAlign: "middle",
                         borderStyle: "none"}}/>
                 <Container maxWidth="md"
-                          style={{
-                              position:"absolute",
-                              left: "50%",
-                              top: "50%",
-                              transform: "translate(-50%, -50%)",
-                              color:"white",
-                              justifyContent:"center",
-                              alignItems:"center"
-                          }}
-                           >
+                          className={classes.container} >
                     <h2 className={classes.titleForm}>Đặt Vé Xe Rẻ</h2>
                     <SearchForm/>
                 </Container>
